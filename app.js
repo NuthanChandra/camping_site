7e4f82a78a3d71570cf15d1cfb33ae3b4444ce93
Rovanova <rovanova.nuthan@gmail.com>
2018-12-03 17:24:41 +0000

    Update nav-bar making it active and add some lines in header.ejs

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var express= require("express"),
 var commentRoutes= require("./routes/comments"),
     campgroundRoutes= require("./routes/campgrounds"),
     indexRoutes= require("./routes/index");
-    
+    console.log(process.env.DATABASEURL);
       mongoose.connect(process.env.DATABASEURL); 
       app.use(bodyParser.urlencoded({extended:true}));
       app.use(flash());
